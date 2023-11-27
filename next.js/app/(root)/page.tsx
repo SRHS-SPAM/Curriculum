e9c22b76d89d4dacb5ffbea6dfb8ba5ce0d065ff
정윤환 <torychu0315@naver.com>
2023-11-27 20:28:07 +0900

    com

--- next.js/app/(root)/page.tsx
+++ next.js/app/(root)/page.tsx
@@ -45,26 +45,114 @@ export default function Home() {
               className="relative w-[35vw]"
               src="/images/배너1.png"
               alt="배너1"
-              layout="fill"
+              width={756}
+              height={419}
             />
           </div>
         </div>
+        {/* <!-- 프론트 강의 --> */}
         <article className="w-[80vw] h-[45vh] flex flex-col justify-between">
-          <div style={{ fontSize: 27, position: "relative", left: "4%" }}>
+          <div className="text-5xl ml-4">
             <strong>프론트엔드 강의</strong>
           </div>
-          <div className="w-[80vw] h-[38vh] flex items-center">
-            <div className="w-[20vw] h-[34vh] bg-[aqua] relative flex items-end rounded-[20px]">
-              <div className="w-[20vw] h-[10vh] bg-[lightgray] rounded-[0px_0px_20px_20px/_0px_0px_20px_20px]" />
+          <div className="bottom1">
+            <div className="Video_box hover:bg-[rgba(0,255,255,0.2)] group">
+              <div className="Video_info trans group-hover:h-[26vh]">
+                <div className="text-[1.2rem]">
+                  누구나 할 수 있다! 프론트 기초 강의 배우기!
+                </div>
+                <div className="Video_subtiitle">
+                  처음부터 배우는 프론트강의!
+                </div>
+                <div className="Video_detail_info">
+                  <p>프론트엔드/코딩</p>
+                  <p>수강 기간 : 11/21 ~ 11/25</p>
+                  <p>대상자 : 정윤환</p>
+                </div>
+              </div>
             </div>
-            <div className="w-[20vw] h-[34vh] bg-[aqua] relative flex items-end rounded-[20px]">
-              <div className="w-[20vw] h-[10vh] bg-[lightgray] rounded-[0px_0px_20px_20px/_0px_0px_20px_20px]" />
+            <div className="Video_box">
+              <div className="Video_info"></div>
             </div>
-            <div className="w-[20vw] h-[34vh] bg-[aqua] relative flex items-end rounded-[20px]">
-              <div className="w-[20vw] h-[10vh] bg-[lightgray] rounded-[0px_0px_20px_20px/_0px_0px_20px_20px]" />
+            <div className="Video_box">
+              <div className="Video_info"></div>
             </div>
-            <div className="w-[20vw] h-[34vh] bg-[aqua] relative flex items-end rounded-[20px]">
-              <div className="w-[20vw] h-[10vh] bg-[lightgray] rounded-[0px_0px_20px_20px/_0px_0px_20px_20px]" />
+            <div className="Video_box">
+              <div className="Video_info"></div>
+            </div>
+            <div className="Video_box">
+              <div className="Video_info"></div>
+            </div>
+          </div>
+        </article>
+        {/* <!-- 백엔드 강의 --> */}
+        <article className="w-[80vw] h-[45vh] flex flex-col justify-between">
+          <div className="text-5xl ml-4">
+            <strong>백엔드 강의</strong>
+          </div>
+          <div className="bottom1">
+            <div className="w-[20vw] h-[34vh] bg-[rgba(0,255,255,1)] relative flex items-end rounded-[20px] group">
+              <div className="w-[20vw] h-[13vh] bg-[#f2f2f2] flex flex-col gap-[1.2rem] font-[bold] transition-[height] duration-500 ease-in-out p-4 rounded-[0px_0px_20px_20px/_0px_0px_20px_20px] group-hover:h-[26vh]">
+                <div className="text-[1.2rem]">
+                  누구나 할 수 있다! 백 기초 강의 배우기!
+                </div>
+                <div className="Video_subtitle">
+                  처음부터 배우는 백엔드강의!
+                </div>
+                <div className="hidden opacity-0 transition-[opacity] duration-500 ease-in-out group-hover:inline group-hover:opacity-100 group-hover:animate-opacity">
+                  {/* detail info*/}
+                  <p>백엔드/코딩</p>
+                  <p>수강 기간 : 11/21 ~ 11/25</p>
+                  <p>대상자 : 국재윤</p>
+                </div>
+              </div>
+            </div>
+            <div className="Video_box">
+              <div className="Video_info"></div>
+            </div>
+            <div className="Video_box">
+              <div className="Video_info"></div>
+            </div>
+            <div className="Video_box">
+              <div className="Video_info"></div>
+            </div>
+            <div className="Video_box">
+              <div className="Video_info"></div>
+            </div>
+          </div>
+        </article>
+        {/* <!-- 디자인 강의 --> */}
+        <article>
+          <a>
+            <strong>디자인 강의</strong>
+          </a>
+          <div className="bottom1">
+            <div className="Video_box">
+              <div className="Video_info">
+                <div className="Video_title">
+                  누구나 할 수 있다! 디자인 기초 강의 배우기!
+                </div>
+                <div className="Video_subtiitle">
+                  처음부터 배우는 디자인강의!
+                </div>
+                <div className="Video_detail_info">
+                  <p>디자인/코딩</p>
+                  <p>수강 기간 : 11/21 ~ 11/25</p>
+                  <p>대상자 : 김마유,전재민</p>
+                </div>
+              </div>
+            </div>
+            <div className="Video_box">
+              <div className="Video_info"></div>
+            </div>
+            <div className="Video_box">
+              <div className="Video_info"></div>
+            </div>
+            <div className="Video_box">
+              <div className="Video_info"></div>
+            </div>
+            <div className="Video_box">
+              <div className="Video_info"></div>
             </div>
           </div>
         </article>
