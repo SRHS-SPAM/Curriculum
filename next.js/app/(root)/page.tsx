01d1fac3c20faea6dd5c2ededa898f5d0e2863f0
정윤환 <torychu0315@naver.com>
2023-11-27 11:26:47 +0900

    fd

--- next.js/app/(root)/page.tsx
+++ next.js/app/(root)/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col m-0 gap-[30px] items-center">
       <header className="fixed z-[1] w-full bg-[white] h-24">
         <div className="w-full h-full flex justify-center items-center gap-12 border-b-[rgba(0,0,0,0.4)] border-b border-solid">
           <Button variant={"ghost"} className="text-[130%]">
@@ -21,49 +21,54 @@ export default function Home() {
           </Button>
         </div>
       </header>
-      <main>
-        <div className="banner_container">
-          <div className="left">
+      <main className="w-[80vw] h-[150vh] flex flex-col justify-between">
+        <div className="h-[40vh] w-[80vw] bg-[#435585] relative flex justify-around rounded-[12.2px] top-40">
+          <div className="h-[40vh] w-[35vw] flex justify-around flex-col">
             <div className="banner_txt_container">
               <p>
-                <strong className="banner_maintxt">
+                <strong className="text-[2.2em] text-[white]">
                   2024년 스팸 프론트 커리큘럼
                 </strong>
               </p>
-              <strong className="banner_subtxt">
+              <strong className="text-2xl text-[white]">
                 하나부터 열까지 배우는 쉬운 과정!
               </strong>
             </div>
-            <div className="banner_page">
+            <div className="bg-[rgba(0,0,0,0.5)] w-3/12 flex justify-around items-center text-center gap-2 text-[white] font-[bold] px-4 py-0 rounded-[2rem]">
               <i className="fa-solid fa-chevron-left" />
               <div className="banner_page_count">1 / 4</div>
               <i className="fa-solid fa-chevron-right" />
             </div>
           </div>
-          <div className="right">
-            <img className="photo" src="img/배너1.png" />
+          <div className="h-[40vh] w-[35vw] flex justify-center items-center">
+            <Image
+              className="relative w-[35vw]"
+              src="/images/배너1.png"
+              alt="배너1"
+              layout="fill"
+            />
           </div>
         </div>
-        <article>
-          <a style={{ fontSize: 27, position: "relative", left: "4%" }}>
+        <article className="w-[80vw] h-[45vh] flex flex-col justify-between">
+          <div style={{ fontSize: 27, position: "relative", left: "4%" }}>
             <strong>프론트엔드 강의</strong>
-          </a>
-          <div className="bottom1">
-            <div className="box1-1">
-              <div className="bar1-1" />
+          </div>
+          <div className="w-[80vw] h-[38vh] flex items-center">
+            <div className="w-[20vw] h-[34vh] bg-[aqua] relative flex items-end rounded-[20px]">
+              <div className="w-[20vw] h-[10vh] bg-[lightgray] rounded-[0px_0px_20px_20px/_0px_0px_20px_20px]" />
             </div>
-            <div className="box1-2">
-              <div className="bar1-2" />
+            <div className="w-[20vw] h-[34vh] bg-[aqua] relative flex items-end rounded-[20px]">
+              <div className="w-[20vw] h-[10vh] bg-[lightgray] rounded-[0px_0px_20px_20px/_0px_0px_20px_20px]" />
             </div>
-            <div className="box1-3">
-              <div className="bar1-3" />
+            <div className="w-[20vw] h-[34vh] bg-[aqua] relative flex items-end rounded-[20px]">
+              <div className="w-[20vw] h-[10vh] bg-[lightgray] rounded-[0px_0px_20px_20px/_0px_0px_20px_20px]" />
             </div>
-            <div className="box1-4">
-              <div className="bar1-4" />
+            <div className="w-[20vw] h-[34vh] bg-[aqua] relative flex items-end rounded-[20px]">
+              <div className="w-[20vw] h-[10vh] bg-[lightgray] rounded-[0px_0px_20px_20px/_0px_0px_20px_20px]" />
             </div>
           </div>
         </article>
       </main>
-    </>
+    </div>
   );
 }
