12e1fb7b9d523eaac1b840916990391964ba1486
igiza1213 <igiza1213@gmail.com>
2024-01-28 15:48:59 +0900

    Update main.tsx

--- next.js/app/(lecture)/[list]/[lecture]/[lectureDetail]/_components/main.tsx
+++ next.js/app/(lecture)/[list]/[lecture]/[lectureDetail]/_components/main.tsx
@@ -3,12 +3,12 @@ import { Move } from "lucide-react";
 const Main = () => {
   return (
     <div className="relative h-full w-full bg-gray-200">
-      <div className="absolute bottom-4 left-4 flex items-end bg-black rounded-xl p-1">
+      {/* <div className="absolute bottom-4 left-4 flex items-end bg-black rounded-xl p-1">
         <Move className="rotate-45 h-[50px] w-[50px]"/>
         <div className="rounded-lg border-2 px-2 py-1 font-semibold text-xs m-2 ml-0">99%</div>
-      </div>
+      </div> */}
     </div>
-  )
-}
+  );
+};
 
 export default Main;
